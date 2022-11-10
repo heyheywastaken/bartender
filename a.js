42d4d1493c5f86cd6276f58aa03886b50dccc9b5
heyheywastaken <100477869+heyheywastaken@users.noreply.github.com>
2022-11-11 01:46:20 +0530

    Delete a.js

--- a.js
+++
@@ -1,7 +0,0 @@
-var input=document.getElementById('searchinput');
-input.addEventListener('keypress',function(event){
-    if (event.key=='Enter'){
-        event.preventDefault();
-        document.getElementById('searchbut').click();
-    }
-});
