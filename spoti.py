39693a25ab4de1431dbc418f36427aaa318ed7b8
LUwUcifer <90955147+LUwUcifer@users.noreply.github.com>
2022-11-27 15:56:01 +0530

    Update spoti.py

--- spoti.py
+++ spoti.py
@@ -11,13 +11,21 @@ sp=spotipy.Spotify(client_credentials_manager=creds)
 search_str = input("Enter an artist: ")
 
 result = sp.search(q = 'artist:' + search_str, type = 'artist')
-url=result['artists']['items'][0]['uri']
+uri=result['artists']['items'][0]['uri']
+name = result['artists']['items'][0]['name']
 
-response = sp.artist_top_tracks(url)
+response = sp.artist_top_tracks(uri)
 
 for track in response['tracks']:
     print(track['name'])
 
+try:
+    related = sp.artist_related_artists(uri)
+    print('Related artists for', name)
+    for artist in related['artists']:
+        print('  ', artist['name'])
+except BaseException:
+    print("usage show_related.py [artist-name]")
 
 # uri=url.split('/')
 # print(uri)
